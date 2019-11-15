3aa380cf38884e3a63a1e8e870f99ce29facaf6f
unknown <dfeuling@gmail.com>
2019-11-15 13:36:12 -0600

    initial successful socket implementation

--- burrisboard/server/Engine.java
+++ burrisboard/server/Engine.java
@@ -1,4 +1,7 @@
 package burrisboard.server;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.sql.*;
 import java.net.ServerSocket;
@@ -17,25 +20,35 @@ class Engine
 
     public static void main(String[] args)
     {
-
-        try
-        {
+        try {
             SQLBridge.connect();
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Error with database connection. Error follows: " + e);
         }
 
-        try
-        {
-            ServerSocket serverSocket = new ServerSocket(3306);
+        try {
+            ServerSocket serverSocket = new ServerSocket(3307);
             Socket clientSocket = serverSocket.accept();
-        }
-        catch(Exception e)
-        {
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+
+            while(true)
+            {
+
+                String clientInput = in.readLine();
+
+                if (clientInput != null) {
+                    out.println("Successfully read by the server!");
+                    System.out.println("Detected input from client.");
+                    System.out.println(clientInput);
+                }
+
+                clientInput = null;
+            }
+        } catch (Exception e) {
             System.out.println("Error with client connection. Error follows: " + e);
         }
+
     }
 
     //create new login
